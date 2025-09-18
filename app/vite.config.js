1f2df13d72e8bce909486ee3725ecae524c66f94
Elmerson <elmerson_350@hotmail.com>
2025-09-18 21:47:54 +0000

    Update Docker configuration to expose port 8019 for Vite server; add local notifications dependency in package.json and package-lock.json; adjust Vite server settings in vite.config.js.

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 8019,
+    https: false
+  },
 })
