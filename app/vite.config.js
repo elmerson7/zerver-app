915a1558a745c1b6b444b3e16fabc12811e663aa
Elmerson <elmerson_350@hotmail.com>
2025-09-22 12:10:04 +0000

    Update Vite server configuration: change host to '0.0.0.0', update port to 8020, and add allowed hosts and polling options for file watching.

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: true,
-    port: 8019,
-    https: false
+    host: '0.0.0.0',
+    port: 8020,
+    strictPort: false,
+    https: false,
+    allowedHosts: ['test.dev.wakuwakuamiguito.com'],
+    watch: {
+      usePolling: true
+    }
   },
 })
